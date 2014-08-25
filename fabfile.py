1a13c333f5c86c824ad6ae421655f9e5dd04bad6
jeremyjbowers <jeremyjbowers@gmail.com>
2014-08-24 22:24:33 -0400

    Handle flat files in www.

--- fabfile.py
+++ fabfile.py
@@ -1,27 +1,38 @@
+import glob
 import json
 import os
 
 from fabric.api import local, require, settings, task
+from fabric.operations import put
 from fabric.state import env
+from fabric.contrib.project import rsync_project
 import gspread
 from termcolor import colored
 
 import app
 
+env.user = "ubuntu"
+env.forward_agent = True
+
+env.hosts = "jeremybowers.com"
+env.settings = None
+
+@task
 def get_wines():
-  gc = gspread.login(os.environ.get('GMAIL_ACCOUNT'), os.environ.get('GMAIL_PASSWORD'))
+  gc = gspread.login(os.environ.get("GMAIL_ACCOUNT"), os.environ.get("GMAIL_PASSWORD"))
   doc = gc.open("Cellar")
   sh = doc.get_worksheet(0)
   wines = sh.get_all_values()
   return [dict(zip(wines[0], w)) for w in wines[1:]]
 
 @task
-def write_wine_json():
+def write_wine_data():
   wines = get_wines()
 
-  with open("data/wine.json", "wb") as writefile:
+  with open("www/wine/wine.json", "wb") as writefile:
     writefile.write(json.dumps(wines))
 
 @task
-def deploy_wine_data():
-  pass
+def deploy():
+  write_wine_data()
+  rsync_project("/var/www/jeremybowers.com", "www/", delete=True)
