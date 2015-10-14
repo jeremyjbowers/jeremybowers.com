aa00a723757bf3134efe3f72e3041e4a96f22e61
Jeremy Bowers <jeremyjbowers@gmail.com>
2015-10-13 22:35:03 -0400

    Overhaul.

--- app.py
+++ app.py
@@ -12,103 +12,115 @@ app = Flask(__name__)
 
 @app.route('/api/v1/')
 def api_controller():
-  with open("data/wine.json", "r") as readfile:
+  with open("www/wine/wine.json", "r") as readfile:
     wines = json.loads(readfile.read())
 
+  payload = []
   for wine in wines:
-    try:
-      wine['cost'] = int(wine['cost'].replace('$', ''))
-    except TypeError:
-      wine['cost'] = None
-  #   try:
-  #     wine['size'] = int(wine['size'].replace('ml', ''))
-  #   except TypeError:
-  #     wine['size'] = None
-    try:
-      wine['year'] = int(wine['year'])
-    except:
-      wine['year'] = str(wine['year'])
-
-  payload = {}
-  count = 0
-  payload['wines'] = []
-
-  from flask import request
-
-  for wine in wines:
-    votes = 0
-
-    if len(dict(request.args).items()) == 0:
-      payload['wines'].append(wine)
-
-    else:
-      for arg in dict(request.args).items():
-        k = arg[0]
-        v = arg[1][-1]
-
-      if "__" in k:
-        k, comparator = k.split("__")
-
-        try:
-          v = int(v)
-        except ValueError:
-          pass
-
-        if comparator == "lte":
-          if wine.get(k, None):
-            if isinstance(v, int) and isinstance(wine.get(k, None), int):
-              if wine.get(k, None) <= v:
-                votes += 1
-              print votes
-
-        if comparator == "gte":
-          if wine.get(k, None):
-            if isinstance(v, int) and isinstance(wine.get(k, None), int):
-              if wine.get(k, None) >= v:
-                votes += 1
-              print votes
-
-        if comparator == "lt":
-          if wine.get(k, None):
-            if isinstance(v, int) and isinstance(wine.get(k, None), int):
-              if wine.get(k, None) < v:
-                votes += 1
-              print votes
-
-        if comparator == "gt":
-          if wine.get(k, None):
-            if isinstance(v, int) and isinstance(wine.get(k, None), int):
-              if wine.get(k, None) > v:
-                votes += 1
-              print votes
-
-        if comparator == "in":
-          if wine.get(k, None):
-            if isinstance(v, str):
-              if v in wine.get(k, None) or not v:
-                votes +=1
-
-        if comparator == "not":
-          if wine.get(k, None):
-            if isinstance(v, str):
-              if v not in wine.get(k, None) or not v:
-                votes += 1
-
-        if comparator == "exact":
-          if wine.get(k, None):
-            if wine.get(k, None) == v or not v:
-              votes += 1
-
+    if wine['consumed']:
+        if wine['wine_type'] == "White":
+            payload.append(1)
+        elif wine['wine_type'] == "Orange":
+            payload.append(-1)
         else:
-          if wine.get(k, None):
-            if wine.get(k, None) == v or not v:
-              votes += 1
+            payload.append(0)
 
-      if len(dict(request.args).items()) == votes:
-        payload['wines'].append(wine)
-
-  payload['count'] = len(payload['wines'])
   return json.dumps(payload)
+
+  # for wine in wines:
+  #   try:
+  #     wine['cost'] = float(wine['cost'].replace('$', ''))
+  #   except TypeError:
+  #     wine['cost'] = None
+  # #   try:
+  # #     wine['size'] = int(wine['size'].replace('ml', ''))
+  # #   except TypeError:
+  # #     wine['size'] = None
+  #   try:
+  #     wine['year'] = int(wine['year'])
+  #   except:
+  #     wine['year'] = str(wine['year'])
+
+  # payload = {}
+  # count = 0
+  # payload['wines'] = []
+
+  # from flask import request
+
+  # for wine in wines:
+  #   votes = 0
+
+  #   if len(dict(request.args).items()) == 0:
+  #     payload['wines'].append(wine)
+
+  #   else:
+  #     for arg in dict(request.args).items():
+  #       k = arg[0]
+  #       v = arg[1][-1]
+
+  #     if "__" in k:
+  #       k, comparator = k.split("__")
+
+  #       try:
+  #         v = int(v)
+  #       except ValueError:
+  #         pass
+
+  #       if comparator == "lte":
+  #         if wine.get(k, None):
+  #           if isinstance(v, int) and isinstance(wine.get(k, None), int):
+  #             if wine.get(k, None) <= v:
+  #               votes += 1
+  #             print votes
+
+  #       if comparator == "gte":
+  #         if wine.get(k, None):
+  #           if isinstance(v, int) and isinstance(wine.get(k, None), int):
+  #             if wine.get(k, None) >= v:
+  #               votes += 1
+  #             print votes
+
+  #       if comparator == "lt":
+  #         if wine.get(k, None):
+  #           if isinstance(v, int) and isinstance(wine.get(k, None), int):
+  #             if wine.get(k, None) < v:
+  #               votes += 1
+  #             print votes
+
+  #       if comparator == "gt":
+  #         if wine.get(k, None):
+  #           if isinstance(v, int) and isinstance(wine.get(k, None), int):
+  #             if wine.get(k, None) > v:
+  #               votes += 1
+  #             print votes
+
+  #       if comparator == "in":
+  #         if wine.get(k, None):
+  #           if isinstance(v, str):
+  #             if v in wine.get(k, None) or not v:
+  #               votes +=1
+
+  #       if comparator == "not":
+  #         if wine.get(k, None):
+  #           if isinstance(v, str):
+  #             if v not in wine.get(k, None) or not v:
+  #               votes += 1
+
+  #       if comparator == "exact":
+  #         if wine.get(k, None):
+  #           if wine.get(k, None) == v or not v:
+  #             votes += 1
+
+  #       else:
+  #         if wine.get(k, None):
+  #           if wine.get(k, None) == v or not v:
+  #             votes += 1
+
+  #     if len(dict(request.args).items()) == votes:
+  #       payload['wines'].append(wine)
+
+  # payload['count'] = len(payload['wines'])
+  # return json.dumps(payload)
 
 # Boilerplate
 if __name__ == '__main__':
